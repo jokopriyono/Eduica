02b404f31d663aac2eabf0e437ba02d64c720d30
Joko Priyono <joe.mandrake007@gmail.com>
2019-11-03 14:34:02 +0700

    Initial commit

---
+++ app/src/main/java/com/developer/eduica/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.developer.eduica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marcinorlowski.fonty.Fonty
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+//        Fonty.setFonts(this)
+    }
+}
