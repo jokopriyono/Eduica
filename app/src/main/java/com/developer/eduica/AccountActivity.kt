77cea24198ebc9503a7567706ad93df37c374f8e
jokopriyono <joe.mandrake007@gmail.com>
2020-07-25 11:05:58 +0700

    ubah intent dan problem background null

--- app/src/main/java/com/developer/eduica/AccountActivity.kt
+++ app/src/main/java/com/developer/eduica/AccountActivity.kt
@@ -1,12 +1,18 @@
 package com.developer.eduica
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_account.*
 
 class AccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        txt_add_account.setOnClickListener {
+            startActivity(Intent(applicationContext, AddAccountActivity::class.java))
+        }
     }
 }
